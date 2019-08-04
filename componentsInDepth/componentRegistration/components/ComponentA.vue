11aceaa7649677449b19dd9285eb8fcde578ed04
fengshiqi1998 <512653808@qq.com>
2019-08-04 16:56:42 +0800

    component registration

---
+++ componentsInDepth/componentRegistration/components/ComponentA.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        ComponentA
+    </div>
+</template>
+<script>
+    export default {
+        name: 'ComponentA'
+    }
+</script>
---
+++ componentsInDepth/componentRegistration/components/ComponentB.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <component-a></component-a>  
+    </div>
+</template>
+<script>
+    // 在模块系统中局部注册
+    import ComponentA from './ComponentA';
+    export default {
+        name: 'ComponentB',
+        components: {
+            ComponentA
+        }
+    }
+</script>
