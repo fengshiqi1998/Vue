2cd277753881b1275367ef06659c63ee179c06d3
fengshiqi1998 <512653808@qq.com>
2019-10-09 18:35:23 +0800

    date 2019/10/9 18:35

--- vuePro/travel/src/pages/home/components/Icons.vue
+++ vuePro/travel/src/pages/home/components/Icons.vue
@@ -1,29 +1,98 @@
 <template>
     <div class="icons">
-        <div class="icon">
-            <div class="icon-img">
-                <img src="" alt="" class="icon-img-content">
-            </div>
-            <div class="icon-desc"></div>
-        </div>
+        <swiper>
+            <swiper-slide v-for="(page, index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img :src="item.imgUrl" alt="" class="icon-img-content">
+                    </div>
+                    <p class="icon-desc">{{item.desc}}</p>
+                </div>
+            </swiper-slide>
+        </swiper>
     </div>
 </template>
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      list: [{
+        id: '001',
+        imgUrl: '',
+        desc: '景点门票'
+      },
+      {
+        id: '002',
+        imgUrl: '',
+        desc: '滑雪季'
+      },
+      {
+        id: '003',
+        imgUrl: '',
+        desc: '泡温泉'
+      },
+      {
+        id: '004',
+        imgUrl: '',
+        desc: '动植园'
+      },
+      {
+        id: '005',
+        imgUrl: '',
+        desc: '景点门票'
+      },
+      {
+        id: '006',
+        imgUrl: '',
+        desc: '滑雪季'
+      },
+      {
+        id: '007',
+        imgUrl: '',
+        desc: '泡温泉'
+      },
+      {
+        id: '008',
+        imgUrl: '',
+        desc: '动植园'
+      },
+      {
+        id: '009',
+        imgUrl: '',
+        desc: '一日游'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.list.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
     @import '~styles/varibles.styl'
-    .icons
-        overflow: hidden
+    @import '~styles/mixins.styl'
+    .icons >>> .swiper-container
         height: 0
         padding-bottom: 50%
-        background: green
+    .icons
+        maring-top: .1rem
         .icon
+            position: relative
             overflow: hidden
             float: left
             width: 25%
+            height: 0
             padding-bottom: 25%
             .icon-img
                 position: absolute
---
+++ vuePro/travel/src/pages/home/components/Recommend.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+<script>
+export default {
+  name: 'HomeRecommand'
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
