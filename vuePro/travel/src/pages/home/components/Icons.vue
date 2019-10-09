ab49e15f18ce01501789c96066ac81e34aa0f7bf
fengshiqi <fengshiqi@zhenrongbao.com>
2019-10-09 16:54:43 +0800

    date 2019/10/9 16:54

---
+++ vuePro/travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="icons">
+        <div class="icon">
+            <div class="icon-img">
+                <img src="" alt="" class="icon-img-content">
+            </div>
+            <div class="icon-desc"></div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .icons
+        overflow: hidden
+        height: 0
+        padding-bottom: 50%
+        background: green
+        .icon
+            overflow: hidden
+            float: left
+            width: 25%
+            padding-bottom: 25%
+            .icon-img
+                position: absolute
+                top: 0
+                left: 0
+                right: 0
+                bottom: .44rem
+                box-sizing: border-box
+                padding: .1rem
+                .icon-img-content
+                    display: block
+                    margin: 0 auto
+                    height: 100%
+            .icon-desc
+                position: absolute
+                left: 0
+                right: 0
+                bottom: 0
+                height: .44rem
+                line-height: .44rem
+                text-align: center
+                color: $darkTextColor
+</style>
---
+++ vuePro/travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item of list" :key="item.id">
+        <img :src="item.imgUrl" alt="" class="swiper-img">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      list: [{
+        id: '001',
+        imgUrl: ''
+      },
+      {
+        id: '002',
+        imgUrl: ''
+      }]
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 31.25%
+        background: #eee
+        .swiper-img
+            width: 100%
+</style>
