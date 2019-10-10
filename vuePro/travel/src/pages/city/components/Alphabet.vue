205ce356c1c41a1e1e24149490a38faf66419061
fengshiqi1998 <512653808@qq.com>
2019-10-10 20:52:30 +0800

    alphabet uncompleted

--- vuePro/travel/src/pages/city/components/Alphabet.vue
+++ vuePro/travel/src/pages/city/components/Alphabet.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="alphabet">aphpabet</div>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">C</li>
+    <li class="item">D</li>
+    <li class="item">E</li>
+    <li class="item">F</li>
+    <li class="item">G</li>
+    <li class="item">H</li>
+  </ul>
 </template>
 <script>
 export default {
@@ -8,4 +17,17 @@ export default {
 </script>
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
+  .list
+    display: flex
+    flex-direction: column
+    jusitfy-content: center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      line-height: .4rem
+      text-align: center
+      color: $bgColor
 </style>
